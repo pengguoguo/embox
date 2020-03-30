20700c329f3b12d6e2ab6e33029d2151d2dfaea4
Anton Bondarev <anton.bondarev2310@gmail.com>
2020-03-30 16:49:00 +0300

    cmds: Clean testing/udp_echo_measurement_test

--- src/cmds/testing/udp_echo_measurement_test/udp_echo_measurement_test.c
+++ src/cmds/testing/udp_echo_measurement_test/udp_echo_measurement_test.c
@@ -12,31 +12,20 @@
 #define NPACK 1024 * 1024
 #define PORT 9930
 
-void diep(char *s)
-{
+static void diep(char *s) {
 	perror(s);
 }
 
-extern uint32_t udp_time, send1_time, udp_id;
-uint32_t after;
-//uint32_t before;
-
-
-
-int last_udp_num[1000000];
-int last_udp;
-
-void dcache_inval(const void *p, size_t size);
 int main(int argc, char **argv) {
 	struct sockaddr_in si_me, si_other;
 	int s, i, slen=sizeof(si_other);
 	char buf[BUFLEN];
 	int port = PORT;
+	int total_packets = 0;
 
-	static int t = 0;
-
-	if ((s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP))==-1)
+	if ((s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP))==-1) {
 		diep("socket");
+	}
 
 	if (argc > 1) {
 		port = atoi(argv[1]);
@@ -47,40 +36,25 @@ int main(int argc, char **argv) {
 	si_me.sin_family = AF_INET;
 	si_me.sin_port = htons(port);
 	si_me.sin_addr.s_addr = htonl(INADDR_ANY);
-	if (bind(s, (void *) &si_me, sizeof(si_me))==-1)
+	if (bind(s, (void *) &si_me, sizeof(si_me))==-1) {
 		return -1;
+	}
 
-	for (i=0; i<NPACK; ) {
-		if (recvfrom(s, buf, BUFLEN, 0, (void *)&si_other, &slen)==-1)
-			diep("recvfrom()");
-
+	for (i = 0; i < NPACK; ) {
 		int num = 0;
+
+		if (recvfrom(s, buf, BUFLEN, 0, (void *)&si_other, &slen)==-1){
+			diep("recvfrom()");
+		}
 
 		sscanf(buf, "This is packet %d", &num);
 
-		last_udp_num[(last_udp++) %1000000] = num;
-	//	printf("got packet!\n");
-	//	clock_gettime(CLOCK_REALTIME, &after);
-
-//		after = *((uint32_t *)0xfffec604);
-//			uint32_t diff = udp_time > after ? udp_time - after : after - udp_time;
-//			if (diff > 270000) {
-//			if (1) printf("<<<%d packed #%d\n", diff, t);
-//			}
-//
-			//udp_id = t;
-		//send1_time = *((uint32_t *)0xfffec604);
 		if (sendto(s, buf, BUFLEN, 0, (void *) &si_other, slen)==-1) {
 			diep("send()");
 		}
-	//	printf("Received packet from %s:%d\nData: %s\n\n",
-	//			inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port), buf);
 
-		t++;
-		if (1 || t % 1000 == 0) {
-			//dcache_inval((void*)0xfffec004, 4);
-			//dcache_inval((void*)0xfffec004, 4);
-//		before = *((uint32_t *)0xfffec004);
+		total_packets++;
+		if (1 || total_packets % 1000 == 0) {
 		}
 	}
 
